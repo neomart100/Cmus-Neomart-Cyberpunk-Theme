d624d6123fba40377cab6b0e944e979a7c81afcf
Neomart <neomart100@gmail.com>
2020-04-30 17:17:17 -0400

    Cmus theme

---
+++ lib.pl
@@ -0,0 +1,78 @@
+/home/neomart/Music/Musica/[AMV] jpn - theme. evangelion.mp3
+/home/neomart/Music/Musica/A phantom pain-MGSV.P.P.mp3
+/home/neomart/Music/Musica/Believer 80remix-imagine dragons.mp3
+/home/neomart/Music/Musica/Born in Spring.mp3
+/home/neomart/Music/Musica/bossa-uh-youtubemp3free.org.mp3
+/home/neomart/Music/Musica/carpenter-brut-anarchy-road-dredd-youtubemp3free.org.mp3
+/home/neomart/Music/Musica/carpenter-brut-escape-from-midwich-valley-youtubemp3free.org.mp3
+/home/neomart/Music/Musica/Carpenter Brut - Looking for Tracy Tzu.mp3
+/home/neomart/Music/Musica/carpenter-brut-roller-mobster-youtubemp3free.org.mp3
+/home/neomart/Music/Musica/Crimewave Crystal Castles (sub esp).mp3
+/home/neomart/Music/Musica/Daisuke.mp3
+/home/neomart/Music/Musica/ Defiance - MGSV T.P.P-The Lost Tapes.mp3
+/home/neomart/Music/Musica/don`t be so serius-low roar.mp3
+/home/neomart/Music/Musica/DOOM (2016) OST - BFG Division.mp3
+/home/neomart/Music/Musica/El Huervo - Rust.mp3
+/home/neomart/Music/Musica/erickD - summer.mp3
+/home/neomart/Music/Musica/Eurythmics - Sweet Dreams (Are Made Of This) (Official Video).mp3
+/home/neomart/Music/Musica/far-cry-3-soundtrack-make-it-bun-dem-youtubemp3free.org.mp3
+/home/neomart/Music/Musica/hisohkah - bike ride.mp3
+/home/neomart/Music/Musica/hisohkah - cherry blossom scatter.mp3
+/home/neomart/Music/Musica/hotline-miami-2-ost-perturbator-technoir-youtubemp3free.org.mp3
+/home/neomart/Music/Musica/hotline-miami-2-wrong-number-soundtrack-divide-youtubemp3free.org.mp3
+/home/neomart/Music/Musica/ i cant fix you-TLT.mp3
+/home/neomart/Music/Musica/jpn - amend..mp3
+/home/neomart/Music/Musica/Le-perv-carpenter-brut.mp3
+/home/neomart/Music/Musica/Lonely Day-System Of A Down .mp3
+/home/neomart/Music/Musica/malo-midnight-child-youtubemp3free.org.mp3
+/home/neomart/Music/Musica/meet matt stryker-carpenter brut.mp3
+/home/neomart/Music/Musica/mercyme-move-audio-youtubemp3free.org.mp3
+/home/neomart/Music/Musica/NARC.mp3
+/home/neomart/Music/Musica/nightmare-avenge sevenfond.mp3
+/home/neomart/Music/Musica/Not in love - Crystal Castles ft. Robert Smith Subtitulada al español.mp3
+/home/neomart/Music/Musica/Not Tomorrow Remix-Silent hill .mp3
+/home/neomart/Music/Musica/ Not Your Kind of People-Garbage.mp3
+/home/neomart/Music/Musica/Obscure II Main Theme .mp3
+/home/neomart/Music/Musica/ Paint It Black-The Rolling Stones .mp3
+/home/neomart/Music/Musica/payday-2-soundtrack-infinite-ammo-youtubemp3free.org.mp3
+/home/neomart/Music/Musica/perturbator-humans-are-such-easy-prey-dangerous-days-official-youtubemp3free.org.mp3
+/home/neomart/Music/Musica/Queen - Another One Bites The Dust (Official Video).mp3
+/home/neomart/Music/Musica/QueenBohemian Rhapsody (Official Video Remastered).mp3
+/home/neomart/Music/Musica/Queen - Don&#39t Stop Me Now (Official Video).mp3
+/home/neomart/Music/Musica/Queen - I Want To Break Free (Official Video).mp3
+/home/neomart/Music/Musica/Queen - We Are The Champions (Official Video).mp3
+/home/neomart/Music/Musica/Queen - We Will Rock You (Official Video).mp3
+/home/neomart/Music/Musica/rock it for me-caravan-palace .mp3
+/home/neomart/Music/Musica/ruiner-game-soundtrack-ost-zamilska-duel-35-youtubemp3free.org.mp3
+/home/neomart/Music/Musica/Scattle - Cardio.mp3
+/home/neomart/Music/Musica/Scattle - From My Heart.mp3
+/home/neomart/Music/Musica/Scattle - It's Safe Now (Official Audio).mp3
+/home/neomart/Music/Musica/scattle-knock-knock-official-audio-youtubemp3free.org.mp3
+/home/neomart/Music/Musica/Scattle - Pie In The Sky.mp3
+/home/neomart/Music/Musica/scattle-remorse-youtubemp3free.org.mp3
+/home/neomart/Music/Musica/Scattle - RGB.mp3
+/home/neomart/Music/Musica/Scattle - Visitors.mp3
+/home/neomart/Music/Musica/Sewerslvt - Drowning In The Sewer (FULL ALBUM).mp3
+/home/neomart/Music/Musica/Sewerslvt - Euphoric Filth (Cheru's Theme).mp3
+/home/neomart/Music/Musica/She-Silent Hill OST.mp3
+/home/neomart/Music/Musica/shot me down-david guetta.mp3
+/home/neomart/Music/Musica/SIAMÉS No Lullaby [Audio & Lyrics].mp3
+/home/neomart/Music/Musica/SIAMÉS Young & Restless [Audio & Lyrics].mp3
+/home/neomart/Music/Musica/silent-hill-2-ost-love-psalm-youtubemp3free.org.mp3
+/home/neomart/Music/Musica/silent-hill-not-tomorrow-metal-on-guitar-lisas-death-youtubemp3free.org.mp3
+/home/neomart/Music/Musica/summer-vivaldi metal.mp3
+/home/neomart/Music/Musica/ Tears Of-Silent Hill OST .mp3
+/home/neomart/Music/Musica/Tetris 99 - Main Theme.mp3
+/home/neomart/Music/Musica/The Man Who Sold The World-MGSV T.P.P- Midge Ure.mp3
+/home/neomart/Music/Musica/True (Piano Version) - Silent Hill 2 [HQ].mp3
+/home/neomart/Music/Musica/wonderland-caravan palace.mp3
+/home/neomart/Music/Musica/04 - Melancholy Requiem.mp3
+/home/neomart/Music/Musica/08 - Silent Circus.mp3
+/home/neomart/Music/Musica/09 - Traversing The Portals Of Reality.mp3
+/home/neomart/Music/Musica/12 - Resting Comfortably.mp3
+/home/neomart/Music/Musica/13 - Nightmarish Waltz.mp3
+/home/neomart/Music/Musica/11 - Cradel Of Forest.mp3
+/home/neomart/Music/Musica/01 - Tender Sugar.mp3
+/home/neomart/Music/Musica/15 - Your Rain.mp3
+/home/neomart/Music/Musica/21 - Room Of Angel.mp3
+/home/neomart/Music/Musica/22 - Waiting For You ~ LIVE AT -Heaven's Night- ~ (unreleased tunes).mp3
